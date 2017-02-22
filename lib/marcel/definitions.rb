1d5c52fcc6ab0f47fa73e01d0f11cb62b99f278c
Tom Ward <tom@popdog.net>
2017-02-22 16:09:50 +0000

    Ensure psb files can be recognised by mimetype

--- lib/marcel/definitions.rb
+++ lib/marcel/definitions.rb
@@ -1,9 +1,3 @@
 require 'mimemagic'
 
-MAGIC_DEFINITIONS = [
-  ["image/vnd.adobe.photoshop", [[0, "8BPS"]]]
-]
-
-MAGIC_DEFINITIONS.each do |(type, magic)|
-  MimeMagic.add(type, magic: magic)
-end
+MimeMagic.add "image/vnd.adobe.photoshop", magic: [[0, "8BPS"]], extensions: %w{ psd psb }
