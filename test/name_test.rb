a2f58ceb344384e5bd04ae6e8536de07a4b4fd4e
Tom Ward <tom@popdog.net>
2017-02-22 16:01:32 +0000

    Ensure file name tests only use the name to get content-type

--- test/name_test.rb
+++ test/name_test.rb
@@ -4,7 +4,7 @@ require 'rack'
 class Marcel::ContentType::NameTest < ActiveSupport::TestCase
   each_content_type_fixture('name') do |file, name, content_type|
     test "gets type for #{content_type} by filename from #{name}" do
-      assert_equal content_type, Marcel::ContentType.for(file, name: name)
+      assert_equal content_type, Marcel::ContentType.for(nil, name: name)
     end
   end
 end
