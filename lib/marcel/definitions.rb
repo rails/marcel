2a9ae3333d15380bd508bd709ee5c97a16c4996c
Tom Ward <tom@popdog.net>
2017-02-23 09:07:22 +0000

    Add tests to ensure .txt and .asc files are recognised as text/plain

--- lib/marcel/definitions.rb
+++ lib/marcel/definitions.rb
@@ -1,4 +1,5 @@
 require 'mimemagic'
 require 'mimemagic/overlay'
 
+MimeMagic.add "text/plain", extensions: %w{ txt asc }
 MimeMagic.add "image/vnd.adobe.photoshop", magic: [[0, "8BPS"]], extensions: %w{ psd psb }
