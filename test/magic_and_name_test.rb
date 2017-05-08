4809919cb0bfe28afc14b0714438fa2cc58c40e2
Tom Ward <tom@popdog.net>
2017-05-08 17:13:33 +0100

    Make it clearer which fixtures are only recognisable by magic and name

--- test/magic_and_name_test.rb
+++ test/magic_and_name_test.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 require 'rack'
 
 class Marcel::MimeType::MagicAndNameTest < ActiveSupport::TestCase
+  # All fixtures that can be recognised by name should also be recognisable when given
+  # the file contents and the name. In some cases, the file contents will point to a
+  # generic type, while the name will choose a more specific subclass
   each_content_type_fixture('name') do |file, name, content_type|
     test "correctly returns #{content_type} for #{name} given both file and name" do
       assert_equal content_type, Marcel::MimeType.for(file, name: name)
--- test/magic_test.rb
+++ test/magic_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 require 'rack'
 
 class Marcel::MimeType::MagicTest < ActiveSupport::TestCase
+  # These fixtures should be recognisable given only their contents. Where a generic type
+  # has more specific subclasses (such as application/zip), these subclasses cannot usually
+  # be recognised by magic alone; their name is also needed to correctly identify them.
   each_content_type_fixture('magic') do |file, name, content_type|
-    test "gets type for #{content_type} by using magic bytes #{name}" do
+    test "gets type for #{content_type} by using only magic bytes #{name}" do
       assert_equal content_type, Marcel::MimeType.for(file)
     end
   end
