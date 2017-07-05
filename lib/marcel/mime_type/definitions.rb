3cb1cafec7807733fec42a39bbfba7bae67852cc
Tom Ward <tom@popdog.net>
2017-07-05 13:47:28 +0100

    Remove unused definition and tidy formatting

--- lib/marcel/mime_type/definitions.rb
+++ lib/marcel/mime_type/definitions.rb
@@ -1,11 +1,9 @@
 require 'mimemagic'
 require 'mimemagic/overlay'
 
-MimeMagic.add "text/plain", extensions: %w{ txt asc }
+MimeMagic.add "text/plain", extensions: %w( txt asc )
 MimeMagic.add "image/vnd.adobe.photoshop", magic: [[0, "8BPS"]], extensions: %w( psd psb )
 
 MimeMagic.add "application/vnd.apple.pages", extensions: %w( pages )
 MimeMagic.add "application/vnd.apple.numbers", extensions: %w( numbers )
 MimeMagic.add "application/vnd.apple.keynote", extensions: %w( key )
-
-MimeMagic.add "application/vnd.android.package-archive", extenstion: %w( apk )
