caedc9e504cc1efbfd1fa119275ea5e8503bfd3f
George Claghorn <george@hey.com>
2021-03-25 20:05:49 -0400

    Add application/vnd.adobe.flash.movie definition

--- lib/marcel/mime_type/definitions.rb
+++ lib/marcel/mime_type/definitions.rb
@@ -38,3 +38,5 @@ Marcel::MimeType.extend "image/heic", magic: [[4, "ftypheic"]], extensions: %w( 
 Marcel::MimeType.extend "video/mp4", magic: [[4, "ftypisom"], [4, "ftypM4V "]], extensions: %w( mp4 m4v )
 
 Marcel::MimeType.extend "font/ttf", magic: [[0, "\x00\x01\x00\x00"]], extensions: %w( ttf ttc )
+
+Marcel::MimeType.extend "application/vnd.adobe.flash.movie", magic: [[0, "FWS"], [0, "CWS"]], extensions: %w( swf )
