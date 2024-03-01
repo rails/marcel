2afc1a1f040710b69f4f5b385df37648320ffef8
Jeremy Daer <jeremy@rubyonrails.org>
2024-03-01 08:55:36 -0800

    Regression fix: binary declared type should fall back to filename extension type (#99)
    The application/octet-stream content type is treated as a default rather
    than a specific declaration. We should check the filename extension for
    the more specific type in this case. If there's no extension, the type
    falls back to binary anyway.
    
    Fixes regression in 1.0.2 -> 1.0.3 introduced by #94.

--- test/declared_type_test.rb
+++ test/declared_type_test.rb
@@ -2,11 +2,16 @@ require 'test_helper'
 require 'rack'
 
 class Marcel::MimeType::DeclaredTypeTest < Marcel::TestCase
-  test "returns declared type as last resort" do
-    assert_equal "text/html", Marcel::MimeType.for(name: "unrecognisable", declared_type: "text/html")
+  test "prefers declared type over filename extension" do
+    assert_equal "text/html", Marcel::MimeType.for(name: "file.txt", declared_type: "text/html")
   end
 
-  test "returns application/octet-stream if declared type empty or unrecognised" do
+  test "prefers filename extension over binary type" do
+    assert_equal "text/plain", Marcel::MimeType.for(name: "file.txt", declared_type: "application/octet-stream")
+  end
+
+  test "defaults to binary if declared type is unrecognized" do
+    assert_equal "application/octet-stream", Marcel::MimeType.for(declared_type: nil)
     assert_equal "application/octet-stream", Marcel::MimeType.for(declared_type: "")
     assert_equal "application/octet-stream", Marcel::MimeType.for(declared_type: "unrecognised")
   end
